4c3ff8a334d6ba0e38b2881250f547b2a85619ab
ItsmeJessMarie <jboll262@gmail.com>
2024-02-06 17:27:51 -0600

    Added basic form handling and added Typewriter plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
+function generateAffirmation(event) {
+  event.preventDefault();
 
+  new Typewriter("#affirmation", {
+    strings: "I am enough",
+    autoStart: true,
+    delay: 50,
+    cursor: "",
+  });
+}
+
+let affirmationFormElement = document.querySelector(
+  "#positive-affirmation-generator-form"
+);
+affirmationFormElement.addEventListener("submit", generateAffirmation);
