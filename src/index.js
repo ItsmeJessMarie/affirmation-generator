aea398bbe820a0c3a447f424b86c045e1f7fd590
ItsmeJessMarie <jboll262@gmail.com>
2024-02-06 20:02:11 -0600

    Updated for better UX

--- src/index.js
+++ src/index.js
@@ -10,16 +10,16 @@ function displayAffirmation(response) {
 function generateAffirmation(event) {
   event.preventDefault();
 
-  let userInstructionsInput = document.querySelector("#user-instructions");
+  let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "1bf547ta2a3986bceb80d3bcaob62269";
   let context =
-    "You are a positive affirmation expert and love to write short positive heartfelt sentences to make people happy. Your mission is to generate a single sentence positive affirmation. Follow example with text centered: <h2>Please repeat this affirmation:</h2><p>I am enough<p>. Make sure to follow the user instructions.";
-  let prompt = `User instructions are: Generate a positive affirmation about ${userInstructionsInput.value}`;
+    "You are a positive affirmation expert and love to write short positive heartfelt sentences to make people happy. Your mission is to generate a single sentence positive affirmation. Follow example with text centered: <h3>Please repeat this affirmation:</h3><p>I am enough<p>. Make sure to follow the user instructions.";
+  let prompt = `User instructions are: Generate a positive affirmation about ${instructionsInput.value}`;
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-  console.log("Generating affirmation");
-  console.log(`Prompt: ${prompt}`);
-  console.log(`Context: ${context}`);
+  let affirmationElement = document.querySelector("#affirmation");
+  affirmationElement.classList.remove("hidden");
+  affirmationElement.innerHTML = `<div class="generating">⏳ Generating a positive affirmation for you about "${instructionsInput.value}"</div>`;
 
   axios.get(apiURL).then(displayAffirmation);
 }
