e92f02ec2cd6c776eba36c6ccb58c22e468f4720
ItsmeJessMarie <jboll262@gmail.com>
2024-02-06 19:22:07 -0600

    Added ai api integration and updated css styles

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
+function displayAffirmation(response) {
+  new Typewriter("#affirmation", {
+    strings: response.data.answer,
+    autoStart: true,
+    delay: 1,
+    cursor: null,
+  });
+}
+
 function generateAffirmation(event) {
   event.preventDefault();
 
-  new Typewriter("#affirmation", {
-    strings: "I am enough",
-    autoStart: true,
-    delay: 50,
-    cursor: "",
-  });
+  let userInstructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "1bf547ta2a3986bceb80d3bcaob62269";
+  let context =
+    "You are a positive affirmation expert and love to write short positive heartfelt sentences to make people happy. Your mission is to generate a single sentence positive affirmation. Follow example with text centered: <h2>Please repeat this affirmation:</h2><p>I am enough<p>. Make sure to follow the user instructions.";
+  let prompt = `User instructions are: Generate a positive affirmation about ${userInstructionsInput.value}`;
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  console.log("Generating affirmation");
+  console.log(`Prompt: ${prompt}`);
+  console.log(`Context: ${context}`);
+
+  axios.get(apiURL).then(displayAffirmation);
 }
 
 let affirmationFormElement = document.querySelector(
